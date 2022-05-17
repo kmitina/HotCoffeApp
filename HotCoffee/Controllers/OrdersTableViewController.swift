9e22e85a0f78be542ed961b64ad0c5a0bdd8aae8
Kris Mitina <kristmitina@gmail.com>
2022-05-17 17:31:58 +0200

    Displaying Orders in UITableView

--- HotCoffee/Controllers/OrdersTableViewController.swift
+++ HotCoffee/Controllers/OrdersTableViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class OrdersTableViewController: UITableViewController {
     
+    var orderListViewModel = OrderListViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,11 +24,12 @@ class OrdersTableViewController: UITableViewController {
         
         let resource = Resource<[Order]>(url: coffeeOrdersURL)
         
-        WebService().load(resource: resource) { result in
+        WebService().load(resource: resource) { [weak self] result in
             
             switch result {
             case .success(let orders):
-                print(orders)
+                self?.orderListViewModel.ordersViewModel = orders.map(OrderViewModel.init)
+                self?.tableView.reloadData()
             case .failure(let error):
                 print(error)
             }
@@ -35,4 +37,21 @@ class OrdersTableViewController: UITableViewController {
         }
     }
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.orderListViewModel.ordersViewModel.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let vm = self.orderListViewModel.orderViewModel(at: indexPath.row)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "OrderTableViewCell", for: indexPath)
+        
+        cell.textLabel?.text = vm.type
+        cell.detailTextLabel?.text = vm.size
+        
+        return cell
+    }
 }
