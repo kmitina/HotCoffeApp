559ff2c92cf3b51a91529e7b57be6c16fddf43e2
Kris Mitina <kristmitina@gmail.com>
2022-05-18 12:12:41 +0200

    Populating Cup Sizes Using View Model

--- HotCoffee/Controllers/AddOrderViewController.swift
+++ HotCoffee/Controllers/AddOrderViewController.swift
@@ -13,12 +13,25 @@ class AddOrderNewController: UIViewController, UITableViewDelegate, UITableViewD
     private var vm = AddCoffeeOrderViewModel()
     
     @IBOutlet weak var tableView: UITableView!
+    private var coffeeSizesSegmentedControl: UISegmentedControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupUI()
+        
 //        self.tableView.delegate = self
 //        self.tableView.dataSource = self
+    }
+    
+    private func  setupUI() {
+        
+        self.coffeeSizesSegmentedControl = UISegmentedControl(items: self.vm.sizes)
+        self.coffeeSizesSegmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(self.coffeeSizesSegmentedControl)
+        self.coffeeSizesSegmentedControl.topAnchor.constraint(equalTo: self.tableView.bottomAnchor, constant: 20).isActive = true
+        self.coffeeSizesSegmentedControl.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
