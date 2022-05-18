d93f3673d14707360df54b475b2becce7ccc8675
Kris Mitina <kristmitina@gmail.com>
2022-05-18 11:36:14 +0200

    Populating Types of Coffee

--- HotCoffee/Controllers/AddOrderViewController.swift
+++ HotCoffee/Controllers/AddOrderViewController.swift
@@ -8,6 +8,28 @@
 import Foundation
 import UIKit
 
-class AddOrderNewController: UIViewController {
+class AddOrderNewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    private var vm = AddCoffeeOrderViewModel()
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        self.tableView.delegate = self
+//        self.tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.vm.types.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CoffeeTypeTableViewCell", for: indexPath)
+        cell.textLabel?.text = self.vm.types[indexPath.row]
+        return cell
+        
+    }
 }
---
+++ HotCoffee/ViewModels/AddCoffeeOrderViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  AddCoffeeOrderViewModel.swift
+//  HotCoffee
+//
+//  Created by DVKSH on 18.05.22.
+//
+
+import Foundation
+
+struct AddCoffeeOrderViewModel {
+    
+    var name: String?
+    var email: String?
+    
+    var types: [String] {
+        return CoffeeType.allCases.map { $0.rawValue.capitalized }
+    }
+    
+    var sizes: [String] {
+        return CoffeeSize.allCases.map { $0.rawValue.capitalized }
+    }
+}
